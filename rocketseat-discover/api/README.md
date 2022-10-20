34c868993f45d5f9404e496e4ebf6a4e75093f24
GleidsonLM <gleidsonlm@gmail.com>
2022-10-20 17:44:37 -0300

    exercise: api

---
+++ rocketseat-discover/api/README.md
@@ -0,0 +1,14 @@
+# API #
+
+> Application Programming Interfaces (APIs) are constructs made available in programming languages to allow developers to create complex functionality more easily. They abstract more complex code away from you, providing some easier syntax to use in its place.
+> https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Introduction
+
+## About ##
+
+Annotations from the Rocketseat Discover learning path.
+URL: https://app.rocketseat.com.br/discover/course/trabalhando-com-apis/
+
+## Tools ##
+- Insomnia
+    > Insomnia is an open-source, cross-platform API client for GraphQL, REST, WebSockets and gRPC.
+    > https://github.com/getinsomnia/insomnia
--- rocketseat-discover/js-async/async.js
+++ rocketseat-discover/js-async/async.js
@@ -14,20 +14,35 @@ import axios from "axios";
 // Print GitHub user full name and repositories with its links.
 async function getUserGitHubInfo(user) {
     // Get GitHub user full name.
-    const userAll = await axios.get(`https://api.github.com/users/+${user}`);
+    const userAll = await axios.get(`https://api.github.com/users/${user}`);
     
     // Get GitHub user repositories.
     const userRepos = await axios.get(userAll.data.repos_url);
     
-    // Print info 
-    console.log(
-        `${userAll.data.name} has ${userRepos.data.length} repositories:`,
-    )
-    // Print public repositories with a link.
-    for(const repo of userRepos.data) {
-        console.log(`${repo.html_url} - ${repo.description}`)
+    // Print user full name and how many public repositories. 
+    if (userRepos.data.length > 1) {
+        console.log(
+            `${userAll.data.name} has ${userRepos.data.length} public repositories:`,
+        )
+    } else if (userRepos.data.length === 1){
+        console.log(
+            `${userAll.data.name} has ${userRepos.data.length} public repository:`,
+        )
+    } else if (userRepos.data.length === 0){
+        console.log(
+            `${userAll.data.name} has no public repositories.`,
+        )
+    } else {
+        console.log(
+            `Couldn't retrieve ${userAll.data.name} public repositories.`,
+        )
     }
-
+    // Print public repositories with a link.
+    if (userRepos.data.length > 0) {
+        for(const repo of userRepos.data) {
+            console.log(`${repo.html_url} - ${repo.description}`)
+        }
+    }
 }
 // Call function getUserGitHubInfo and catch exceptions
-getUserGitHubInfo('gleidsonlm').catch(err => console.log(err.message))
+getUserGitHubInfo('WordPress').catch(err => console.log(err.message))
